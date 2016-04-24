68d897fad570124abbbe08e97dbd7ad855c4522e
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-04-24 16:54:29 +0100

    backbone for displaying the outputs from the police api queries

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+var lng = '-1.131592';
+var lat = '52.629729';
+var date;
+var crime_categories;
+
+request('https://data.police.uk/api/crimes-street/all-crime?lat='+lat+'&'+'lng='+lng, function (error, response, body) {
+ if (!error && response.statusCode == 200) { 
+   var output = JSON.parse(body);
+   crime_categories = output[0].category;
+ } else {
+   console.log(error);
+ }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Paw Map' });
+  res.render('index', { title: 'Paw Map', crime_categories: crime_categories });
 });
 
 module.exports = router;
