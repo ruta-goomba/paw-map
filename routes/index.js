2eed5950ef060cc89d01c75a299db914180825d8
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-04-24 17:31:51 +0100

    add the numbers of crimes committed in each category

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,19 @@ var lng = '-1.131592';
 var lat = '52.629729';
 var date;
 var crime_categories = [];
+var no_of_crimes_in_category = [];
 
 request('https://data.police.uk/api/crimes-street/all-crime?lat='+lat+'&'+'lng='+lng, function (error, response, body) {
  if (!error && response.statusCode == 200) { 
    var output = JSON.parse(body);
-   console.log(output.length);
    for (i=0; i<output.length; i++) {
-     if (crime_categories.indexOf(output[i].category) == -1){
-       crime_categories.push(output[i].category);
+     var current_category = output[i].category;
+     var index_of_category = crime_categories.indexOf(current_category)
+     if (index_of_category == -1){
+       crime_categories.push(current_category);
+       no_of_crimes_in_category[crime_categories.length-1] = 1;
+     } else {
+       no_of_crimes_in_category[index_of_category] += 1;
      }
    }
  } else {
@@ -23,7 +28,7 @@ request('https://data.police.uk/api/crimes-street/all-crime?lat='+lat+'&'+'lng='
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Paw Map', crime_categories: crime_categories });
+  res.render('index', { title: 'Paw Map', crime_categories: crime_categories, no_of_crimes_in_category: no_of_crimes_in_category });
 });
 
 module.exports = router;
