b8498f29e4a6eeee9f90a940211deb1f539dee78
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-04-24 17:09:10 +0100

    returning a list of crimes in a specified area

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,17 @@ var request = require('request');
 var lng = '-1.131592';
 var lat = '52.629729';
 var date;
-var crime_categories;
+var crime_categories = [];
 
 request('https://data.police.uk/api/crimes-street/all-crime?lat='+lat+'&'+'lng='+lng, function (error, response, body) {
  if (!error && response.statusCode == 200) { 
    var output = JSON.parse(body);
-   crime_categories = output[0].category;
+   console.log(output.length);
+   for (i=0; i<output.length; i++) {
+     if (crime_categories.indexOf(output[i].category) == -1){
+       crime_categories.push(output[i].category);
+     }
+   }
  } else {
    console.log(error);
  }
