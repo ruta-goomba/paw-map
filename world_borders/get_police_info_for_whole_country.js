117949032531dd0860ca8f61363c88ea2b7373b8
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-05-08 16:33:12 +0100

    adding a script to create a uk coordinate grid with crime objects

--- world_borders/get_police_info_for_whole_country.js
+++ world_borders/get_police_info_for_whole_country.js
@@ -11,19 +11,6 @@ var endpoint;
 var total_length;
 var counter=0;
 
-/*var police_api_callback = function (error, response, body) {
-    console.log('executing... total length is '+ total_length + ' and counter is ' + counter++);
-      if (!error && response.statusCode == 200) {
-        var output = JSON.parse(body);
-        console.log('request output is '+output);
-        uk_crimes_grid_2016_01.geometry.coordinates.push([lat, lng]);
-        uk_crimes_grid_2016_01.geometry.crimes.push(output);
-        //fs.writeFileSync(path.join(__dirname,'uk_crimes_grid_2016_01.json'), JSON.stringify(uk_crimes_grid_2016_01, null, 2) , 'utf-8');
-      } else {
-        console.log(error);
-      }
-  }*/
-
 (function query_police_api() {
   fs.readFile(path.join(__dirname, 'uk_cleaned_grid.json'), 'utf8', function (err, data) {
     if (err) throw err;
@@ -39,6 +26,10 @@ var counter=0;
           console.log('request output is '+output);
           uk_crimes_grid_2016_01.geometry.coordinates.push(coords);
           uk_crimes_grid_2016_01.geometry.crimes.push(output);
+          if (counter%5000 === 0 || counter ===total_length-1){
+            fs.writeFileSync(path.join(__dirname,'uk_crimes_grid_2016_01_'+counter+'.json'), JSON.stringify(uk_crimes_grid_2016_01, null, 2) , 'utf-8');
+            uk_crimes_grid_2016_01 = {"geometry": {"coordinates": [],"crimes": []}};
+          }
           callback();
         } else {
           console.log(error);
@@ -48,11 +39,10 @@ var counter=0;
       if (err) {
         console.log('failed to process');
       } else {
-        fs.writeFileSync(path.join(__dirname,'uk_crimes_grid_2016_01.json'), JSON.stringify(uk_crimes_grid_2016_01, null, 2) , 'utf-8');
-        console.log('created crimes grid file');
+        //fs.writeFileSync(path.join(__dirname,'uk_crimes_grid_2016_01.json'), JSON.stringify(uk_crimes_grid_2016_01, null, 2) , 'utf-8');
+        console.log('created crimes grid files');
       }
     });
   });
 }());
 
-//query_police_api();
