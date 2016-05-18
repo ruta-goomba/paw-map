e439b3d82ee19834dcffa5d457744297934e5ab5
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-05-18 22:06:38 +0100

    removing a bunch of files and modifying 02_generate_country_polygon.js to make it easily reusable for any country

--- helpers/create_country_grid/js/02_generate_country_polygon.js
+++ helpers/create_country_grid/js/02_generate_country_polygon.js
@@ -1,7 +1,9 @@
 var turf = require('turf'),
-	path = require('path');
+	path = require('path'),
+	fs = require('fs');
 
-function createRectangleEnclosingTheCountry(lng1, lng2, lat1, lat2, path_to_file){
+
+function createRectangleEnclosingTheCountry(lng1, lng2, lat1, lat2, file_name){
 	var polygon = turf.polygon([[
  		[lng1, lat1],
  		[lng2, lat1],
@@ -10,10 +12,10 @@ function createRectangleEnclosingTheCountry(lng1, lng2, lat1, lat2, path_to_file
  		[lng1, lat1]
 	]]);
 
-	fs.writeFileSync(path_to_file, JSON.stringify(polygon, null, 2) , 'utf-8');
+	fs.writeFileSync(path.resolve('../geojson', file_name), JSON.stringify(polygon, null, 2) , 'utf-8');
 
 }
 
 //UK polygon
-createRectangleEnclosingTheCountry(-8.65, 1.77, 49.86, 60.86, path.resolve('geojson', 'uk_polygon2.geojson'));
+createRectangleEnclosingTheCountry(-8.65, 1.77, 49.86, 60.86, 'uk_polygon2.geojson');
 
