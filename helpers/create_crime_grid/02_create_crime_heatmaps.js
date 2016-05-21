0a5c8b5dfd1faf4c03dc90b65c4a93c227018f24
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-05-21 23:08:52 +0100

    implemented heatmap creation logic

--- helpers/create_crime_grid/02_create_crime_heatmaps.js
+++ helpers/create_crime_grid/02_create_crime_heatmaps.js
@@ -1,60 +1,33 @@
-var ReadStream = require('./file_readable_stream.js');
-var crime_data = ReadStream(path.resolve('..', 'uk_crimes_grid_2016_01_20000.json');
+var path = require('path'),
+    JSONStream = require('JSONStream'),
+    all_crime_categories = ['violent-crime', 'anti-social-behaviour', 'vehicle-crime', 'other-crime', 'theft-from-the-person', 'shoplifting', 'robbery', 'public-order', 'possession-of-weapons', 'other-theft', 'drugs', 'criminal-damage-arson', 'burglary', 'bicycle-theft'];
 
-var WriteStream = require('./file_writable_stream.js');
-var crime_heatmap = WriteStream(path.join(__dirname, 'tmp_heatmap.json'));
+function filter_crime_data(country, date, crime_category){
+  var ReadStream = require('../streams/file_readable_stream.js'),
+      crime_data = ReadStream(path.resolve('../create_crime_grid/json/'+country+'/'+date+'/all_crimes', country+'_crimes_grid_'+date+'_test.json'));
+  
+  var TransformStream = require('../streams/extract_crime_categories_transform_stream.js'),
+      crime_category_grid = TransformStream(crime_category);
 
-crime_data.pipe(crime_heatmap); 
+  var WriteStream = require('../streams/file_writable_stream.js'),
+      crime_category_heatmap = WriteStream(path.resolve('../create_crime_grid/json/'+country+'/'+date+'/selected_crimes', crime_category+'_test_heatmap.json'));
 
-/*var crime_grid_files = ['5000', '10000', '15000', '20000', '25000', '30000', '35000', '40000', '45000', '50000', '55000', '60000', '65000', '70000', '75000', '80000', '84249']
-//var all_crime_categories = ['violent-crime', 'anti-social-behaviour', 'vehicle-crime', 'other-crime', 'theft-from-the-person', 'shoplifting', 'robbery', 'public-order', 'possession-of-weapons', 'other-theft', 'drugs', 'criminal-damage-arson', 'burglary', 'bicycle-theft'];
-var all_crime_categories = ['other-crime'];
-var uk_crime_grid;
-var counter = 0;
-var latitude;
-var longitude;
-var crime_heatmap = [];
+  crime_data
+  .pipe(JSONStream.parse('crimes'))
+  .pipe(crime_category_grid)
+  .pipe(crime_category_heatmap);
+} 
 
-function get_crime_grid(crime){
-  var writeStream = fs.createWriteStream(path.join(__dirname, crime+'_heatmap.json'), {encoding: 'utf8'});
-  for (var n=0; n<crime_grid_files.length; n++){
-    var readStream = fs.createReadStream(path.resolve('..', 'uk_crimes_grid_2016_01_'+crime_grid_files[n]+'.json'), {encoding: 'utf8'});
-    readStream
-    .on('data', function (data) {
-      if (!uk_crime_grid){
-      	uk_crime_grid = data;
-      } else {
-      	uk_crime_grid += data;
-      }
-   	})
-   	.on('end', function(){
-   	  try {
-   	  	//console.log(uk_crime_grid);
-   	  	uk_crime_grid = JSONStream.parse(uk_crime_grid);
-	    for (var i=0; i<uk_crime_grid.geometry.crimes.length; i++){
-		  if (uk_crime_grid.geometry.crimes[i].length > 0){
-		    for (var j=0; j<uk_crime_grid.geometry.crimes[i].length; j++){
-		      if (uk_crime_grid.geometry.crimes[i][j].category === crime){
-		        counter++;
-		      }	
-		    }
-		    if (counter > 0) {
-		      crime_heatmap.push({location: uk_crime_grid.geometry.coordinates[i], weight: counter});
-		    }
-		    counter = 0;
-		  } 
-		}
-      } catch (err) {
-	      console.log(err);
-	  }
-   	});
+// runs out of memory when run for multiple crime categories
+function create_heatmaps_for_all_crimes(crime_categories, country, date){
+  for (i=0; crime_categories.length;i++){
+    filter_crime_data(country, date, crime_categories[i]);
   }
-  readStream.pipe(writeStream);
-};
+}
 
-(function generate_grids(){
-  for (var i=0; i< all_crime_categories.length; i++) {
-	get_crime_grid(all_crime_categories[i]);
-	console.log('crime '+ all_crime_categories[i] +' grid created');
-  }
-}());*/
+//uk
+//create_heatmaps_for_all_crimes(test_crime_categories, 'uk', '2016-01');
+filter_crime_data('uk', '2016-01', 'other-theft');
+
+
+
---
+++ helpers/streams/extract_crime_categories_transform_stream.js
@@ -0,0 +1,76 @@
+var Transform = require('stream').Transform; 
+var inherits = require('util').inherits;
+var async = require('async');
+
+
+function JSONClean(content, options) {  
+  if ( ! (this instanceof JSONClean))
+    return new JSONClean(content, options);
+
+  if (! options) options = {};
+  options.objectMode = true;
+  Transform.call(this, options);
+  this.crime_category = content;
+}
+
+inherits(JSONClean, Transform);
+
+var output = {"location":undefined, weight:0};
+var counter = 0;
+var first_write = true;
+
+JSONClean.prototype._transform = function _transform(obj, encoding, callback) {  
+  var self = this;
+  var total_length = obj.length;
+  var prev_limit = 0;
+  var iter_limits = Array.apply(null, {length: total_length}).map(Number.call, Number);
+  async.each(iter_limits, function(limit, callback_one){
+    try{
+      async.each(obj.slice(prev_limit, limit), function(coord, inner_callback){
+        prev_limit = limit;
+        if (coord[1][0]){
+          for (i=0; i<coord[1].length; i++){
+            if (coord[1][i].category === self.crime_category){
+              output.location=coord[0];
+              counter++;
+            }
+          }
+        }
+        inner_callback();
+      }, function(err){
+        if (err){
+          console.log('failed to process');
+        } else {
+          if (limit === 0){
+            self.push('[');
+          } 
+          if (counter>0){
+            output.weight = counter;
+            if (!first_write){
+              self.push(','+JSON.stringify(output));
+            } else {
+              self.push(JSON.stringify(output));
+              first_write = false;
+            }
+          }
+          if (limit === total_length-1){
+            setTimeout(function(){self.push(']')}, 2000);
+          }
+          console.log(counter);
+          counter = 0;
+        }
+      });
+    } catch(err){
+      return callback(err);
+    }
+  }, function(error_one){
+    if(error_one){
+      console.log('failed to estimate limits');
+    } else {
+      callback_one();
+      callback()
+    }
+  })
+};
+
+module.exports = JSONClean;
