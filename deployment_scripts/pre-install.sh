b88c99cfce49b2aaf9ee2a0abe427515dfe19974
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-06-12 21:50:24 +0100

    adding dependacy installation to pre-install.sh

--- deployment_scripts/pre-install.sh
+++ deployment_scripts/pre-install.sh
@@ -1,1 +1,7 @@
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm cache clean -f 
+sudo npm install -g n
+sudo n 6.2.0
+sudo ln -sf /usr/local/n/versions/node/6.2.0/bin/node /usr/bin/node
 cd /
