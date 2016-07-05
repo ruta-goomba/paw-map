9c825db24d0dd1cf6ff5306ba805ea27ca6f4087
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-07-05 23:28:29 +0100

    per crime files generated

--- helpers/create_crime_grid/03_create_crime_heatmaps_per_crime.js
+++ helpers/create_crime_grid/03_create_crime_heatmaps_per_crime.js
@@ -2,14 +2,13 @@ var async = require('async'),
     fs = require('fs'),
     path = require('path'),
     all_heat_maps = ['violent-crime', 'anti-social-behaviour', 'vehicle-crime', 'other-crime', 'theft-from-the-person', 'shoplifting', 'robbery', 'public-order', 'possession-of-weapons', 'other-theft', 'drugs', 'criminal-damage-arson', 'burglary', 'bicycle-theft'],
-    subset_heat_maps = ['violent-crime', 'anti-social-behaviour'],
     all_dates = ['2015-05','2015-06', '2015-07', '2015-08', '2015-09', '2015-10', '2015-11', '2015-12', '2016-01', '2016-02', '2016-03', '2016-04'],
     heatmap_data = {},
     read_path = 'json/uk/dates/',
     write_path = 'json/uk/crimes/';
 
 function read_in_heatmap_data() {
-  async.eachSeries(subset_heat_maps, get_crime_type_info, final_callback);
+  async.eachSeries(all_heat_maps, get_crime_type_info, final_callback);
 }
 
 var get_crime_type_info = function(heatmap, callback){
