135f9064ac3c7ec7c0a800bebe6d2e677c94813c
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-07-06 17:00:30 +0100

    including all the new data in the page

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,12 @@ var crime_categories = [];
 var no_of_crimes_in_category = [];
 
 var police_api_callback = function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
       var output = JSON.parse(body);
-      for (i=0; i<output.length; i++) {
-        var current_category = output[i].category;
-        var index_of_category = crime_categories.indexOf(current_category)
+      var current_category, index_of_category;
+      for (var i=0; i<output.length; i+= 1) {
+        current_category = output[i].category;
+        index_of_category = crime_categories.indexOf(current_category);
         if (index_of_category == -1){
           crime_categories.push(current_category);
           no_of_crimes_in_category[crime_categories.length-1] = 1;
@@ -32,7 +33,7 @@ var police_api_callback = function (error, response, body) {
     } else {
       console.log(error);
     }
-  }
+  };
 
 function query_police_api(callback) {
   request('https://data.police.uk/api/crimes-street/all-crime?lat='+lat+'&'+'lng='+lng, police_api_callback)
@@ -43,25 +44,20 @@ function query_police_api(callback) {
 ////////////////////////////////////////////////////////
 
 var all_heat_maps = ['violent-crime', 'anti-social-behaviour', 'vehicle-crime', 'other-crime', 'theft-from-the-person', 'shoplifting', 'robbery', 'public-order', 'possession-of-weapons', 'other-theft', 'drugs', 'criminal-damage-arson', 'burglary', 'bicycle-theft'];
-var subset_of_maps = ['violent-crime', 'anti-social-behaviour'];
 var all_dates = ['2015-05','2015-06', '2015-07', '2015-08', '2015-09', '2015-10', '2015-11', '2015-12', '2016-01', '2016-02', '2016-03', '2016-04'];
 var heatmap_data = {};
 var core_path = 'helpers/create_crime_grid/json/uk/crimes/';
 
 function read_in_heatmap_data(callback) {
-  async.each(subset_of_maps, function(heatmap, cb){
+  async.eachSeries(all_heat_maps, function(heatmap, cb){
     fs.readFile(core_path +heatmap+'.json', 'UTF8', function(err, data){
-      if (err) {throw err};
+      if (err) {throw err}
       heatmap_data[heatmap] = JSON.parse(data);
-      if (heatmap_data[heatmap]['2016-04']){console.log('data found')};
       cb();
     });
   }, function(err){
-    if (err) {
-      throw err
-    } else {
-      callback(null);
-    }
+    if (err) {throw err}
+    callback(null);
   });
 }
 
@@ -74,27 +70,27 @@ async.series(work, done);
 
 
 function done(err) {
-  if (err) throw err;
+  if (err) {throw err}
   router.get('/', function(req, res, next) {
-    res.render('index', { 
+    res.render('index', {
       title: 'Paw Map',
-      all_dates: all_dates, 
-      crime_categories: crime_categories, 
+      all_dates: all_dates,
+      crime_categories: crime_categories,
       no_of_crimes_in_category: no_of_crimes_in_category,
       violent_crime_heatmap_data: heatmap_data['violent-crime'],
-      anti_social_behaviour_heatmap_data: heatmap_data['anti-social-behaviour']
-      // vehicle_crime_heatmap_data: heatmap_data['vehicle-crime'],
-      // other_crime_heatmap_data: heatmap_data['other-crime'],
-      // theft_from_person_heatmap_data: heatmap_data['theft-from-the-person'],
-      // shoplifting_heatmap_data: heatmap_data['shoplifting'],
-      // robbery_heatmap_data: heatmap_data['robbery'],
-      // public_order_heatmap_data: heatmap_data['public-order'],
-      // possession_of_weapons_heatmap_data: heatmap_data['possession-of-weapons'],
-      // other_theft_heatmap_data: heatmap_data['other-theft'],
-      // drugs_heatmap_data: heatmap_data['drugs'],
-      // arson_heatmap_data: heatmap_data['criminal-damage-arson'],
-      // burglary_heatmap_data: heatmap_data['burglary'],
-      // bicycle_theft_heatmap_data: heatmap_data['bicycle-theft']
+      anti_social_behaviour_heatmap_data: heatmap_data['anti-social-behaviour'],
+      vehicle_crime_heatmap_data: heatmap_data['vehicle-crime'],
+      other_crime_heatmap_data: heatmap_data['other-crime'],
+      theft_from_person_heatmap_data: heatmap_data['theft-from-the-person'],
+      shoplifting_heatmap_data: heatmap_data['shoplifting'],
+      robbery_heatmap_data: heatmap_data['robbery'],
+      public_order_heatmap_data: heatmap_data['public-order'],
+      possession_of_weapons_heatmap_data: heatmap_data['possession-of-weapons'],
+      other_theft_heatmap_data: heatmap_data['other-theft'],
+      drugs_heatmap_data: heatmap_data['drugs'],
+      arson_heatmap_data: heatmap_data['criminal-damage-arson'],
+      burglary_heatmap_data: heatmap_data['burglary'],
+      bicycle_theft_heatmap_data: heatmap_data['bicycle-theft']
     });
   })
 }
