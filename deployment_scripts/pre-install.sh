cf8f9dd8f6fc5e6e6c2b85cf3b95b21aaf2688fc
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-08-09 16:05:25 +0100

    adding scripts to reduce space usage in vms

--- deployment_scripts/pre-install.sh
+++ deployment_scripts/pre-install.sh
@@ -3,10 +3,12 @@ if [ -d /pawmap ]; then
 fi
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm cache clean -f 
+sudo npm cache clean -f
 sudo npm install -g n
 sudo n 6.2.0
 sudo ln -sf /usr/local/n/versions/node/6.2.0/bin/node /usr/bin/node
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt-get clean
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo rm /etc/nginx/sites-available/default
