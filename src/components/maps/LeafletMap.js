06739f0f2edacd4d59000b90c90a2007cac0d90a
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-09-02 19:26:42 +0100

    moving back to google maps heat map

--- src/components/maps/LeafletMap.js
+++ src/components/maps/LeafletMap.js
@@ -1,37 +1,31 @@
 import React, {PropTypes} from 'react';
-import { Map, Marker} from 'react-leaflet';
-import { divIcon, point } from 'leaflet';
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import GoogleMap from 'google-map-react';
 import LoadingDots from '../common/LoadingDots';
-import {GoogleLayer} from 'react-leaflet-google';
 
 /* eslint-disable react/jsx-no-bind */
 
-const default_position = [52.629729, -1.131592];
+const default_position = {lat: 52.629729, lng: -1.131592};
 const text = '...';
+//googlekey="AIzaSyD1K2OVJWLAZvnNT6Ae_ZOdsE9F_9uTBLY"
 
 const LeafletMap = ({points, date, hotspots, loading, map_styles}) => {
   return (
     <div className="section__map" style={{height:map_styles.height +'px'}}>
       {(loading) ? <LoadingDots/> : null}
-      <Map center={[52.629729, -1.131592]} zoom={6}>
-        <HeatmapLayer
-          points={points}
-          longitudeExtractor={m => m['location'][0]}
-          latitudeExtractor={m => m['location'][1]}
-          intensityExtractor={m => m['weight']}
-          blur={0.01}
-          radius={0.01}
-          max={(hotspots.length>0) ? hotspots[0]['weight'] : 3}
-        />
-        <GoogleLayer googlekey="AIzaSyD1K2OVJWLAZvnNT6Ae_ZOdsE9F_9uTBLY"  type="ROADMAP"/>
-        {[0, 1, 2].map(pos =>
-          <Marker key={Math.random()}
-                  position={(hotspots.length>0) ? [hotspots[pos]['location'][1],hotspots[pos]['location'][0]] : default_position}
-                  icon={divIcon({className: 'section__map__marker--crime-hot-spot', html:(hotspots.length>0)  ? (hotspots[pos]['weight']).toString() : text, iconSize: point(40, 40)})}
-          />
-        )}
-      </Map>
+      <GoogleMap
+        defaultCenter={default_position}
+        defaultZoom={6}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({map, maps}) => {
+          console.log(points[0]);
+          const heatmap = new maps.visualization.HeatmapLayer({
+            data: points.map(point => (
+              {location: new google.maps.LatLng(point['location'][1], point['location'][0]),
+              weight: point['weight']}))
+          });
+          heatmap.setMap(map);
+        }}
+      ></GoogleMap>
     </div>
   );
 };
