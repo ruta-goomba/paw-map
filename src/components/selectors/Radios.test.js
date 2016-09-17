d2cca55502119f7d286c7be6216bbf76b939b6aa
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-09-17 15:18:41 +0100

    new Radios component tests

--- src/components/selectors/Radios.test.js
+++ src/components/selectors/Radios.test.js
@@ -1,15 +1,18 @@
 import expect from 'expect';
 import React from 'react';
 import {mount, shallow} from 'enzyme';
+import sinon from 'sinon';
 import Radios from './Radios';
 
-function setup() {
-  const category_array=['comp-first', 'comp-second'];
-  return shallow(<Radios categories={category_array}/>);
-}
-
 it( 'renders Radios component', () => {
-  const wrapper = setup();
+  // Setup
+  const category_array=['comp-first', 'comp-second'];
+  let selected = 'comp-first';
+  const onRadioChange = sinon.spy();
+  const wrapper = shallow(<Radios categories={category_array} selected={selected} onRadioChange={onRadioChange}/>);
+  // Tests
   expect(wrapper.find('form').children().length).toBe(2);
   expect(wrapper.find('form').children().at(1).text()).toEqual('comp second');
+  expect(wrapper.find('.section__form--radio').at(1).children().at(0).simulate('change'));
+  expect(onRadioChange.calledOnce).toEqual(true);
 });
