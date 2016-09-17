1440f0e2a4be8e6b44742f31aa86a8b15d64b796
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-09-17 15:58:44 +0100

    more tests for Info component

--- src/components/common/Info.test.js
+++ src/components/common/Info.test.js
@@ -3,13 +3,15 @@ import React from 'react';
 import {mount, shallow} from 'enzyme';
 import Info from './Info';
 
-function setup() {
-  return shallow(<Info text="some text" link="http://someplace.com"/>);
-}
-
-it( 'renders Info component', () => {
-  const wrapper = setup();
+it( 'renders Info component with link', () => {
+  const wrapper = shallow(<Info text="some text" link="http://someplace.com"/>);
   expect(wrapper.find('div').children().length).toBe(1);
   expect(wrapper.find('p').text()).toInclude('some text');
   expect(wrapper.find('a').prop('href')).toBe('http://someplace.com');
+});
+
+it( 'renders Info component without link', () => {
+  const wrapper = shallow(<Info text="some text"/>);
+  expect(wrapper.find('div').children().length).toBe(1);
+  expect(wrapper.find('p').text()).toEqual('some text.');
 });
