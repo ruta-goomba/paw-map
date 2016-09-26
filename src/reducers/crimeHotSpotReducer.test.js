cec81dc5ca2b6830363c6b5d6e29efa79c69c8eb
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-09-26 20:46:14 +0100

    modifying crimeHotSpotReducer tests to reduce repetition

--- src/reducers/crimeHotSpotReducer.test.js
+++ src/reducers/crimeHotSpotReducer.test.js
@@ -2,15 +2,19 @@ import expect from 'expect';
 import crimeHotSpotReducer from './crimeHotSpotsReducer';
 import * as actions from '../actions/crimeActions';
 
+function setup(){
+  return [
+    {location: ['-8.11', '54.48'], weight:2},
+    {location: ['-9.11', '53.27'], weight:1},
+    {location: ['-8.79', '54.98'], weight:5}
+  ];
+}
+
 describe('Crime Hot Spot Reducer', () => {
   it('should load crime hot spots for crime category selected by default when passed LOAD_HOT_SPOTS_INITIAL_SUCCESS', () => {
     //setup
     const initialState = [];
-    const newCrimeHotSpots = [
-      {location: ['-8.11', '54.48'], weight:2},
-      {location: ['-9.11', '53.27'], weight:1},
-      {location: ['-8.79', '54.98'], weight:5}
-    ];
+    const newCrimeHotSpots = setup();
     const action = actions.loadCrimeHotSpotsInitialSuccess(newCrimeHotSpots);
 
     //act
@@ -23,11 +27,7 @@ describe('Crime Hot Spot Reducer', () => {
   it('should load crime hot spots for the selected crime category when passed LOAD_HOT_SPOTS_SUCCESS', () => {
     //setup
     const initialState = [];
-    const newCrimeHotSpots = [
-      {location: ['-8.11', '54.48'], weight:2},
-      {location: ['-9.11', '53.27'], weight:1},
-      {location: ['-8.79', '54.98'], weight:5}
-    ];
+    const newCrimeHotSpots = setup();
     const action = actions.loadCrimeHotSpotsSuccess(newCrimeHotSpots);
 
     //act
