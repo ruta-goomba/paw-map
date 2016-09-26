31a3a150320e15dff01715dd78d8ff9c715fd77a
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2016-09-26 18:02:03 +0100

    crime hot spot reducer tests

---
+++ src/reducers/crimeHotSpotReducer.test.js
@@ -0,0 +1,40 @@
+import expect from 'expect';
+import crimeHotSpotReducer from './crimeHotSpotsReducer';
+import * as actions from '../actions/crimeActions';
+
+describe('Crime Hot Spot Reducer', () => {
+  it('should load crime hot spots for crime category selected by default when passed LOAD_HOT_SPOTS_INITIAL_SUCCESS', () => {
+    //setup
+    const initialState = [];
+    const newCrimeHotSpots = [
+      {location: ['-8.11', '54.48'], weight:2},
+      {location: ['-9.11', '53.27'], weight:1},
+      {location: ['-8.79', '54.98'], weight:5}
+    ]
+    const action = actions.loadCrimeHotSpotsInitialSuccess(newCrimeHotSpots);
+
+    //act
+    const newState = crimeHotSpotReducer(initialState, action);
+
+    //assert
+    expect(newState.length).toEqual(3);
+    expect(newState[0].weight).toEqual(2);
+  });
+  it('should load crime hot spots for the selected crime category when passed LOAD_HOT_SPOTS_SUCCESS', () => {
+    //setup
+    const initialState = [];
+    const newCrimeHotSpots = [
+      {location: ['-8.11', '54.48'], weight:2},
+      {location: ['-9.11', '53.27'], weight:1},
+      {location: ['-8.79', '54.98'], weight:5}
+    ]
+    const action = actions.loadCrimeHotSpotsSuccess(newCrimeHotSpots);
+
+    //act
+    const newState = crimeHotSpotReducer(initialState, action);
+
+    //assert
+    expect(newState.length).toEqual(3);
+    expect(newState[0].weight).toEqual(2);
+  });
+});
